841d48f90a211fa42384260f44a7230398f751a2
Marcelo <marcelo.bracet1@gmail.com>
2024-11-05 12:38:24 -0300

    fix: remove Contentful client configuration file

--- src/lib/contentful.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'contentful';
-
-export const client = createClient({
-  space: 'u3zg67qw0rf2',
-  accessToken: 'CFPAT-jHtsejN0GNLcJy9aLfbT3YWQbC-n8s5UtNb0gZ_EMXI',
-});
-
